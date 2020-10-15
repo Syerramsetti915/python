d03afc7cd0c09bea47866212975a6a0121d5a80f
suseelkumar915 <yerramsetti915@gmail.com>
2020-10-15 01:05:32 +0000

    t

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-w = open("h.txt") 
+#w = open("/2ndfile/h.txt") 
+import github3
+import github3.models
+
+url='https://github.com/skumaryer/'
+
+def authGit(args):
+    gh = github3.login(username=skumaryer, password=b3FimCXJHVqGUTwRYpQAfBMR, url='https://github.com/skumaryer/')
+    return gh
+    w = open('test.txt','r')
+    if w >=1:
+        print (w.read())
+    else:
+        print ('no file exists')
